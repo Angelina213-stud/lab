e0b9834172a3731bffd90662912b5c62b3b5ab61
Angelina <diachuk.anhelina@chnu.edu.ua>
2025-11-05 18:54:11 +0200

    Lab2

--- courseSection/m02_variables_datatypes_concatenation/FlightTicket/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/FlightTicket/src/Task.java
@@ -2,6 +2,12 @@ public class Task {
     public static void main(String[] args) {
 
         // Write your solution here
+        String city1 = "Chicago";
+        String city2 = "San Fransisco";
+        double oneWayFare = 643.5;
 
+        char DOLLAR = 36;
+
+        System.out.println("From " + city1 + " to " + city2 + " one way cost is " + DOLLAR + oneWayFare);
     }
 }
